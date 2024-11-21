00ad3d5170103947ac11b94f281eacd660d2cbb5
Yina Noguera <ylnoguera@iemsantateresita.edu.co>
2024-11-21 09:02:17 -0500

    ejercicio base de datos

---
+++ 11/semana 6/database.sql
@@ -0,0 +1,60 @@
+DROP DATABASE MUSICA;
+
+CREATE DATABASE musica;
+
+CREATE TABLE Artista(
+    id int(3)PRIMARY KEY,
+    nombres varchar(100),
+    apellidos varchar(100),
+    nombre_artistico varchar(100),
+    fecha_nacimiento date,
+    sexo enum ("masculino", "femenino", "otro")
+     
+);
+
+CREATE TABLE genero_musical(
+    id int(3)PRIMARY KEY,
+    nombre varchar(100),
+    descripcion varchar(300)
+    
+);
+    
+CREATE TABLE artista_genero(
+    id_genero_musical int(3) NOT NULL,
+    id_artista int(3) NOT NULL,
+    FOREIGN KEY (id_genero_musical) REFERENCES genero_musical(id),
+    FOREIGN KEY (id_artista) REFERENCES artista(id)
+    
+);
+
+CREATE TABLE cancion (
+    id int(3) PRIMARY KEY,
+    titulo varchar(100),
+    duracion int(4),
+    año_de_lanzamiento date,
+    valoracion int(1)
+
+);
+
+CREATE TABLE album(
+    id int(3)PRIMARY KEY,
+    año int(4),
+    nombre varchar(100),
+    caratula BLOB
+
+);
+
+CREATE TABLE cancion_album(
+    id_cancion int(3) NOT NULL,
+    id_album int(3) NOT NULL,
+    FOREIGN KEY (id_cancion) REFERENCES cancion(id),
+    FOREIGN KEY (id_album) REFERENCES album(id)
+    
+);
+
+CREATE TABLE cancion_genero(
+    id_genero_musical int(3) NOT NULL,
+    id_cancion int(3) NOT NULL,
+    FOREIGN KEY (id_genero_musical) REFERENCES genero_musical(id),
+    FOREIGN KEY (id_cancion) REFERENCES cancion(id)
+);
